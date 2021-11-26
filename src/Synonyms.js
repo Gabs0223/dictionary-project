061f39d8b514853f1a38426424411a5f20ceb721
Gabs0223 <sanchezgabriela23@gmail.com>
2021-11-25 18:23:02 -0600

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function Synonyms(props) {
   console.log(props.synonyms);
-  if (props.synonyms.lenght !== 0) {
+  if (!props.synonyms.length) {
+    return null;
+  } else {
     return (
       <div className="Synonyms">
         <strong>Synonyms:</strong>
@@ -11,12 +13,6 @@ export default function Synonyms(props) {
             return <li key={i}>{synonym}</li>;
           })}
         </ul>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <br />
       </div>
     );
   }
