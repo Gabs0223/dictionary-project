189740d1e142312626ea0f1b8fcf9970db2f248a
Gabs0223 <sanchezgabriela23@gmail.com>
2021-11-29 13:50:28 -0600

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,11 @@ export default function Photos(props) {
           {props.photos.map(function (photo, i) {
             return (
               <div className="col-4" key={i}>
-                <img src={photo.src.landscape} className="img-fluid" />
+                <img
+                  src={photo.src.landscape}
+                  className="img-fluid"
+                  crossOrigin="anonymous"
+                />
               </div>
             );
           })}
